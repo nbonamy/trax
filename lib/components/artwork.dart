faf791211236179a14e5b834075081085ecf6c63
Nicolas Bonamy <nicolas@Bonamy.fr>
2023-04-14 18:12:49 -0500

    refresh fixes

--- lib/components/artwork.dart
+++ lib/components/artwork.dart
@@ -1,4 +1,3 @@
-import 'dart:developer';
 import 'dart:typed_data';
 
 import 'package:flutter/cupertino.dart';
@@ -8,21 +7,16 @@ class ArtworkWidget extends StatelessWidget {
   final double size;
   final double radius;
   final Color placeholderBorderColor;
-  final String? logTag;
   const ArtworkWidget({
     super.key,
     required this.bytes,
     required this.size,
     this.radius = 8.0,
     this.placeholderBorderColor = CupertinoColors.lightBackgroundGray,
-    this.logTag,
   });
 
   @override
   Widget build(BuildContext context) {
-    if (logTag != null) {
-      log('$logTag: ${bytes?.lengthInBytes ?? 0}');
-    }
     return bytes == null
         ? Container(
             width: size,
